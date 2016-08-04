1ba93a9c4c82f264e78d5a1faf209971d53d630e
Buzzbumble <s.kang919@gmail.com>
2016-08-04 12:16:13 -0700

    bleh

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,7 +5,7 @@ class PropertiesController < ApplicationController
     @properties = Property.paginate(:page => params[:page], :per_page => 10)
     respond_to do |format|
       format.html
-      format.js 
+      format.js
       format.json do
         render json: Property.all.to_json(include: [:address])
       end
