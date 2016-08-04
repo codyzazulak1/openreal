d48e7dbeae2c0a649e19d393db3898a5fc3c7cfa
Patrick Peng <me@patrickpeng.ca>
2016-08-04 11:11:18 -0700

    fix footer bg color bug

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -8,22 +8,24 @@ $(window).scroll(function(){
   var bottom_pos = $('.similar-listings').offset().top - $('.floating-box').outerHeight() - paddingHeight * 2;
   // console.log(cur_pos, bottom_pos);
   var floatOn = $('.photo-carousel').outerHeight() + $('nav').outerHeight() + $('.property-overview').outerHeight() - floatOffset;
-  // debugger
-  if (cur_pos > floatOn && cur_pos < bottom_pos && !$(".floating-box").hasClass('is-floating')) {
-    $(".floating-box").addClass('is-floating');
-    $(".floating-box.is-floating").width( $(".floating-box").parent().width() );
-  } 
-  else if (cur_pos > floatOn && cur_pos >= bottom_pos && $(".floating-box").hasClass('is-floating')) {
-    $(".floating-box").addClass('bottom').removeClass('is-floating').css('top', (bottom_pos - $('.floating-box').outerHeight()) + floatOffset - paddingHeight - 2);
-  }
-  else if (cur_pos > floatOn && cur_pos < bottom_pos) {
-    $(".floating-box").addClass('is-floating').removeClass('bottom').removeAttr('style');
-    $(".floating-box.is-floating").width( $(".floating-box").parent().width() );
-    // console.log('remove');
-  }
-  else {
-    if (cur_pos < floatOn && $(".floating-box").hasClass('is-floating')) {
-      $(".floating-box").removeClass('is-floating').removeClass('bottom').removeAttr('style');;
+  
+  if ($(window).width() >= 640) {
+    if (cur_pos > floatOn && cur_pos < bottom_pos && !$(".floating-box").hasClass('is-floating')) {
+      $(".floating-box").addClass('is-floating');
+      $(".floating-box.is-floating").width( $(".floating-box").parent().width() );
+    } 
+    else if (cur_pos > floatOn && cur_pos >= bottom_pos && $(".floating-box").hasClass('is-floating')) {
+      $(".floating-box").addClass('bottom').removeClass('is-floating').css('top', (bottom_pos - $('.floating-box').outerHeight()) + floatOffset - paddingHeight - 2);
+    }
+    else if (cur_pos > floatOn && cur_pos < bottom_pos) {
+      $(".floating-box").addClass('is-floating').removeClass('bottom').removeAttr('style');
+      $(".floating-box.is-floating").width( $(".floating-box").parent().width() );
+      // console.log('remove');
+    }
+    else {
+      if (cur_pos < floatOn && $(".floating-box").hasClass('is-floating')) {
+        $(".floating-box").removeClass('is-floating').removeClass('bottom').removeAttr('style');;
+      }
     }
   }
 });
