d139babe6545bdddc5a047c86407bf725101d5ee
Patrick Peng <me@patrickpeng.ca>
2016-08-05 12:33:47 -0700

    update new listings section on landing page

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -1,13 +1,15 @@
 //= require fancybox
 
 $(window).scroll(function(){
-  // current position
-  var cur_pos = $(this).scrollTop();
-  var paddingHeight = Number($('.property-details').css('padding-top').replace("px", ""));
-  var floatOffset = Number($('.floating-container').css('margin-top').replace("px", "").replace("-", ""));
-  var bottom_pos = $('.similar-listings').offset().top - $('.floating-box').outerHeight() - paddingHeight * 2;
-  // console.log(cur_pos, bottom_pos);
-  var floatOn = $('.photo-carousel').outerHeight() + $('nav').outerHeight() + $('.property-overview').outerHeight() - floatOffset;
+  if ($('body.properties.show').length) {
+    // current position
+    var cur_pos = $(this).scrollTop();
+    var paddingHeight = Number($('.property-details').css('padding-top').replace("px", ""));
+    var floatOffset = Number($('.floating-container').css('margin-top').replace("px", "").replace("-", ""));
+    var bottom_pos = $('.similar-listings').offset().top - $('.floating-box').outerHeight() - paddingHeight * 2;
+    // console.log(cur_pos, bottom_pos);
+    var floatOn = $('.photo-carousel').outerHeight() + $('nav').outerHeight() + $('.property-overview').outerHeight() - floatOffset;
+  }
   
   if ($(window).width() >= 640) {
     if (cur_pos > floatOn && cur_pos < bottom_pos && !$(".floating-box").hasClass('is-floating')) {
