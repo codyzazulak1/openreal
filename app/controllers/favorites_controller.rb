645572846504c540859ca82d2390e28da70b136e
Buzzbumble <s.kang919@gmail.com>
2016-08-04 17:39:48 -0700

    Favorites can be created

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,6 +5,17 @@ class FavoritesController < ApplicationController
     @favorites = @customer.favorites
   end
 
+  def create
+    @customer = Customer.find(params[:customer_id])
+    @property = Property.find(params[:pid])
+
+    @favorite = Favorite.new(customer: @customer, property: @property)
+
+    if @favorite.save
+
+    end
+  end
+
   private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
                       }
 
   resources :customers, :only => [:show] do
-    resources :favorites, :only => [:index]
+    resources :favorites
   end
 
   get '/dashboard', action: :get_dash, controller: 'dashboard'
@@ -28,8 +28,6 @@ Rails.application.routes.draw do
   post 'properties/sell', action: :sell, controller: 'properties'
 
   resources :properties do
-    post '/favorite', action: :favorite, controller: 'properties'
-    delete '/favorite', action: :unfavorite, controller: 'properties'
     collection do
       get 'cities'
     end
