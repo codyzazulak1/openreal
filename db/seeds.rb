1f4e3215f99bac1c7d5c738e26cfdc284cd9d211
Buzzbumble <s.kang919@gmail.com>
2016-10-17 10:33:33 -0700

    seeds.rb setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create(first_name: 'Steven', email: 'stevenk@openreal.ca', password: 'skopenadmin', password_confirmation: 'skopenadmin')
+Admin.create(first_name: 'Fio', email: 'fiorellal@openreal', password: 'flopenadmin', password_confirmation: 'flopenadmin')
+Admin.create(first_name: 'Cody', email: 'codyz@openreal.ca', password: 'czopenadmin', password_confirmation: 'czopenadmin')
+
+customer_submitted = ["Unappraised", "Awaiting Response", "Closing"]
+agent_submitted = ["Unapproved", "Approved"]
+owned_properties = ["Unlisted", "Listed", "Archived"]
+
+customer_submitted.each do |u|
+  Status.create(category: "Customer Submitted", name: u)
+  puts "Customer Submitted ------ #{u}"
+end
+
+agent_submitted.each do |u|
+  Status.create(category: "Agent Submitted", name: u)
+  puts "Agent Submitted ------ #{u}"
+end
+
+owned_properties.each do |u|
+  Status.create(category: "Owned Properties", name: u)
+  puts "Owned Properties ------ #{u}"
+end
+
+arr =
+[
+  "bathroom",
+  "pool",
+  "basement",
+  "kitchen"
+]
+
+arr.each do |u|
+
+  Upgrade.create(name: u)
+
+end
