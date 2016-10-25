566ba93a2b04d7e0160e4d1cbb2bbb5529cdb9c3
FioFiyo <fioarlego1@msn.com>
2016-10-25 23:59:25 +0000

    changed banner layout for agents page

--- app/models/agent_form.rb
+++ app/models/agent_form.rb
@@ -1,2 +1,5 @@
 class AgentForm < ActiveRecord::Base
+	validates :full_name, length: {minimum: 2}, presence: true
+	validates :email, length: {minimum: 8}, presence: true
+	validates :company_name, presence: true
 end
