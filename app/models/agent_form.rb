959ecd68f972fb1081a2d3f9129e920bed952754
FioFiyo <fioarlego1@msn.com>
2016-10-26 00:10:41 +0000

    added agents to footer and edit style h6 for agentspg

--- app/models/agent_form.rb
+++ app/models/agent_form.rb
@@ -1,5 +1,3 @@
 class AgentForm < ActiveRecord::Base
-	validates :full_name, length: {minimum: 2}, presence: true
-	validates :email, length: {minimum: 8}, presence: true
-	validates :company_name, presence: true
+
 end
