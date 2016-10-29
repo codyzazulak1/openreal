588bb5ccc48dcf3cd7ec7a1634055261c56b14a1
FioFiyo <fioarlego1@msn.com>
2016-10-29 01:12:32 +0000

    added listings method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -50,4 +50,12 @@ class WelcomeController < ApplicationController
     @aform = AgentForm.new
   end
 
+  def listings
+    @just_listed = Property.where('created_at >= ?', 2.days.ago)
+    @featured = Property.all.sample(8)
+    @new_properties = Property.just_listed(8)
+    @properties_p = Property.all
+  end
+
+
 end
