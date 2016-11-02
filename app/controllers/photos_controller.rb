52cac9edd34f03df95d0ae7f0ab6f33890caacbf
FioFiyo <fioarlego1@msn.com>
2016-11-02 09:24:11 +0000

    added last end missing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,10 @@
 class PhotosController < ApplicationController
 
-def update
-  respond_to do |format|
-    if @photos.update(photos_params)
-      format.html { redirect_to dashboard_properties_path, notice: 'Photo was successfully updated.' }
-    end 
-  end
+	def update
+	  respond_to do |format|
+	    if @photos.update(photos_params)
+	      format.html { redirect_to dashboard_properties_path, notice: 'Photo was successfully updated.' }
+	    end 
+	  end
+	end
 end
