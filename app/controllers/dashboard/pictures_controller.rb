e38d852fa1b9644f2f29cc9452f8e180f1964aa9
Buzzbumble <s.kang919@gmail.com>
2016-11-24 17:40:17 -0800

    Fixed upload and delete

--- app/controllers/dashboard/pictures_controller.rb
+++ app/controllers/dashboard/pictures_controller.rb
@@ -4,11 +4,16 @@ class Dashboard::PicturesController < ApplicationController
 
   def create
 
-      add_more_pictures(pictures_params[:pictures])
-      flash[:error] += "Failed to upload" unless @property.save
+      if pictures_params[:deletes]
+        remove_pictures_at_index(pictures_params[:deletes].map! {|u| u.to_i})
+      end
 
-      remove_pictures_at_index(params[:deletes].map! {|u| u.to_i})
-      flash[:error] += "Failed to delete image(s)" unless @property.save
+      if pictures_params[:pictures]
+        add_more_pictures(pictures_params[:pictures])
+      end
+
+      @property.save
+
       redirect_to edit_dashboard_property_path(@property)
 
   end
@@ -23,22 +28,20 @@ class Dashboard::PicturesController < ApplicationController
   end
 
   def add_more_pictures(new_pictures)
-    return @property.pictures if new_pictures.nil?
     pictures = @property.pictures
     pictures += new_pictures
     @property.pictures = pictures
   end
 
   def remove_pictures_at_index(index_arr)
-    return @property.pictures if index_arr.nil? || index_arr.empty?
     remain_pictures = @property.pictures
-    deleted_pictures = remain_pictures.delete_if.with_index { |r, i| index_arr.include? i } # delete the target image
+    deleted_pictures = remain_pictures.dup - remain_pictures.delete_if.with_index { |r, i| index_arr.include? i } # delete the target image
     deleted_pictures.each {|i| i.try(:remove!) }
     @property.pictures = remain_pictures
   end
 
   def pictures_params
-    params.require(:property).permit({pictures: []})
+    params.require(:property).permit({pictures: [], deletes: []})
   end
 
 end
