88a83d437f602a09ea694fb514a88c04264d17d1
FioFiyo <fioarlego1@msn.com>
2017-04-11 04:23:16 +0000

    agent sessions set

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,35 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def agent_setup 
+    if resource_class == Agent
+      @agent = Agent.new(sign_up_params)
+      session[:agent_params] = {
+        first_name: @agent.first_name,
+        last_name: @agent.last_name,
+        company_name: @agent.company_name
+      }
+
+      redirect_to new_agent_registration_path
+    end
+  end
+
+  def new
+    @agent = Agent.new(session[:agent_params])
+    session.delete(:agent_params)
+    
+    # No access to create a new admin
+    if resource_class == Admin
+      redirect_to new_admin_session_path
+    end
+  end
+
   private
 
     def sign_up_params
       if resource_class == Admin
         params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       elsif resource_class == Agent
-        params.require(:agent).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        params.require(:agent).permit(:first_name, :last_name, :email, :password, :password_confirmation, :company_name).except!(:agent_id)
       elsif resource_class == Customer
         params.require(:customer).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
@@ -16,7 +39,7 @@ class RegistrationsController < Devise::RegistrationsController
       if resource_class == Admin
         params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       elsif resource_class == Agent
-        params.require(:agent).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        params.require(:agent).permit(:first_name, :last_name, :email, :password, :password_confirmation, :company_name)
       elsif resource_class == Customer
         params.require(:customer).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
