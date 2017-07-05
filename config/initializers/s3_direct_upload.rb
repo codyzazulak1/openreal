e156783f6b9254aab585a7c74a05f8a7320f8da7
FioFiyo <fioarlego1@msn.com>
2017-07-05 22:18:19 +0000

    removed config for previous gem s3 direct upload since new one is in place

--- config/initializers/s3_direct_upload.rb
+++
@@ -1,6 +0,0 @@
-S3DirectUpload.config do |c|
-	c.acces_key_id = ENV['AWS_ACCESS_KEY_ID']
-	c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-	c.bucket = ENV['AWS_BUCKET_NAME']
-	c.region = ENV['AWS_REGION']
-end
