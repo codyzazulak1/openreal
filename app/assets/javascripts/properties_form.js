1c4418370296d24b7e492c5532832bef03f2e315
FioFiyo <fioarlego2@gmail.com>
2017-10-12 09:06:06 +0000

    Adjust when user chooses none of the above. Will diselect any previously selected items

--- app/assets/javascripts/properties_form.js
+++ app/assets/javascripts/properties_form.js
@@ -15,11 +15,19 @@ $(document).ready(function(){
 
 			if ($(this).is(':checked')){
 				$('.cb-sell').each(function(){$(this).removeAttr('required')})
+				
+	
+				if ($('input[type=checkbox]').last()){
+					if ($('input[type=checkbox]').last().is(':checked')){			
+						$('.cb-sell').prop('checked', false)
+						$('input[type=checkbox]').last().prop('checked', true)
+					}
+				} 
 			}
 			else if ($('input:checkbox:checked').length == 0) {
 				$('.cb-sell').each(function(){$(this).attr('required', true)})
-			}	
-	})	
+			}
+	})
 
 })
 
