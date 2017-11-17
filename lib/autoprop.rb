8ad7ac60817dd9207de45d8ffb0bf7edcfb6b4ad
FioFiyo <fioarlego2@gmail.com>
2017-11-17 11:43:06 +0000

    moved task to method to iterate over comparables

--- lib/autoprop.rb
+++ lib/autoprop.rb
@@ -20,22 +20,19 @@ module Autoprop
 
 		browser.text_field(name: 'login').set!("#{login}")
 
-
 	  browser.text_field(name: 'password').set!("#{p}")
 		
-		puts browser.url
-
 		browser.button(type: 'submit').click
 
 		search_url = 'https://app.autoprop.ca/property/search'
 		
 		browser.goto search_url
-		
+			
 		if (browser.div(class: 'modal-footer').exists? && browser.div(class: 'modal-alert').exists?)
-			browser.div(class: 'modal-footer').button(text: 'New Search').click
-			browser.text_field(id: 'lookup').wait_until_present
+			browser.div(class: 'modal-footer').button(text: 'New Search').click	
 		end
-		
+	
+		browser.text_field(id: 'lookup').wait_until_present	
 		browser.text_field(id: 'lookup').set!("#{self.strip(address)[0]}")
 		
 		sleep 2
@@ -48,7 +45,6 @@ module Autoprop
 
 		browser.div(class: 'address-name').wait_until_present
 
-		browser.window.resize_to(1480, 850)
 
 		#Menu Autopro
 		browser.input(class: ["chosen-search-input"], index: 2).send_keys :tab
@@ -64,30 +60,108 @@ module Autoprop
 		sleep 2 
 		browser.li(class: ["active-result"], index: 0).click
 
+		puts "Generate report page"	
 
+	#	browser.window.resize_to(1480, 850)
+	#	sleep 2
 
-		sleep 2
-		browser.screenshot.png
-		browser.screenshot.save 'a.png'
-
-		puts "Im on generate report page"	
-		sleep 2
-
+	#	browser.screenshot.png
+	#	browser.screenshot.save 'a.png'		
 		
 		browser.div(class: 'navigation-bar').button(class: ['btn', 'btn-primary']).click
 		
-		puts browser.url
-		
-		puts "Gathering data"
 		
 
 		#Paragon residential comparables 
 		puts "comparables"
-		comparable_container = browser.div(class: "paragon-res", index: 1).table(class: "table")
-		puts comparable_container
-		comparable_container.wait_until_present
+
+		browser.trs(class: 'pbi-avoid').wait_until_present
+
+		row_s = browser.trs(class: 'pbi-avoid')
+		
+		comparables(row_s)
+
+		#begin 
+		#	h = browser.trs(:class => 'pbi-avoid')[0].tds.each_with_index{|cel, i|
+
+		#	items = {
+		#		sold_price:	 '',
+		#		list_price:  '',
+		#		list_date:	 ''
+		#	}
+
+		#	for (b = 0, b < 21, b++){
+		#		if (b == (6..11) && i == b)
+		#			case i
+		#			when 6
+		#				sold_price: cel.text
+		#			when 7
+		#				list_price: cel.text
+		#			when 8
+		#				list_date: cel.text
+		#			end
+		#		end
+
+		#	}
+		#	return items
+		#	}
+		#rescue Exception => msg
+		#	puts msg	
+		#	puts "1. First section FAILED"
+
+	#	ensure
+	# end
+	
+		return assessment = {
+			total_value_today: 'test',
+			assessed_date: 'test',
+			assessed_date_value: 'test',
+			land_value: 'test'
+		}
+	end
 
 
+	def	self.strip(address)
+		address = address.split(',')
+		return address
+	end
+
+	def	comparables(rows)
+
+		collection = rows.map.with_index { |r, i|
+		
+			e_r = []
+
+			as = {
+				sold_price: 's',
+				list_price: 's',
+				list_date:	's'
+			}
+			thi = r[i].map.with_index { |dat, x|
+		
+				if 		x == 6
+					sold_price = dat.text
+				elsif x == 7
+					list_price = dat.text
+				elsif x == 8
+					list_date  = dat.text
+				else
+					next
+				end
+
+				as[:sold_price] = sold_price
+				as[:list_price] = list_price
+				as[:list_date]	= list_date
+				
+			}
+			return thi
+		}
+		puts "Comparables finished..."
+		puts collection
+	end
+
+	def rest
+	
 		browser.div(id: 'propertyValues').wait_until_present
 		assess_div = browser.div(id: 'propertyValues')
 		total_assess = assess_div.div(id: 'totalAssessed')
@@ -133,11 +207,6 @@ module Autoprop
 		browser.close
 		puts "Assessment: #{assessment}"
 		return assessment
-	end
-
-
-	def	self.strip(address)
-		address = address.split(',')
-		return address
+	
 	end
 end	
